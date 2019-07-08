ca513e3c056d050a4597d67cb83e450079661522
Abdulla Thanseeh <abdulla.thanseeh@wizmobi.com>
2019-07-08 11:00:59 +0530

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Abdulla Thanseeh on 7/8/19.
+//  Copyright © 2019 Abdulla Thanseeh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
